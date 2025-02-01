07bc159eb6ec07671a346b623444561cb900607c
Alex Timms <at421@ic.ac.uk>
2025-02-01 17:05:47 +0000

    add: tool call graph

---
+++ backend/agentic/graph.py
@@ -0,0 +1,39 @@
+from langgraph.graph import StateGraph, MessagesState, START, END
+from utils import ClaudeClient
+from langgraph.prebuilt import ToolNode
+from langchain_core.prompts import SystemMessagePromptTemplate
+
+class AgenticWorkflow:
+
+  def __init__(self, tools):
+      self.agent = ClaudeClient()
+      self.tools = tools
+      self.model_with_tools = self.agent.client.bind_tools(tools)
+      self.tool_node = ToolNode(self.tools)
+
+  def should_continue(self, state: MessagesState):
+      messages = state["messages"]
+      last_message = messages[-1]
+      if last_message.tool_calls:
+          return "tools"
+      return END
+
+  def call_model(self, state: MessagesState):
+      print("Calling model")
+      messages = state["messages"]
+      response = self.model_with_tools.invoke(messages)
+      print(response)
+      return {"messages": [response]}
+
+  def compile_graph(self):
+    workflow = StateGraph(MessagesState)
+
+    workflow.add_node("agent", self.call_model)
+    workflow.add_node("tools", self.tool_node)
+
+    workflow.add_edge(START, "agent")
+    workflow.add_conditional_edges("agent", self.should_continue, ["tools", END])
+    workflow.add_edge("tools", "agent")
+
+    app = workflow.compile()
+    return app

ff9ecf4ee230b9a10582b00d4f8a5edf8fb3af20
Alex Timms <at421@ic.ac.uk>
2025-02-01 22:21:23 +0000

    add: app

--- backend/agentic/app.py
+++ backend/agentic/app.py
@@ -1,15 +1,15 @@
 from langgraph.prebuilt import ToolNode
-from tools import list_dir, make_change_to_file, create_dir, create_file
+from tools import list_dir, make_change_to_file, create_file
 from graph import AgenticWorkflow
 
-tools = [list_dir, make_change_to_file, create_dir, create_file]
+tools = [list_dir, make_change_to_file, create_file]
 
 PM_PROMPT = """
-You are a Project Manager, who is tasked with breaking down a request into changes required to the project structure.
-You have access to a set of tools to explore the project structure and make changes to the project.
-When making a change, explain to the programmer what changes to make to the code, do not write any code yourself.
+You are tasked with breaking down a request into changes required in a NextJS app router project.
+You have access to a set of tools to explore the project structure and make changes to the project dependant on the user query.
+The first tool you must use is 'list_dir', which lists the contents of a directory. Only request changes to tsx files do not change the globals.css.
+When making a change, explain to the programmer what changes to make to the code, do not write any code yourself, keep explaination direct to tasks do not expand at all.
 """
-
 
 if __name__ == "__main__":  
     workflow = AgenticWorkflow(tools)
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     app.invoke({
         "messages": [
             {"role": "system", "content": PM_PROMPT},
-            {"role": "user", "content": "Add a button that says 'Click Me' to the home page."},
+            {"role": "user", "content": "Add a help page, with a button with a link to www.ichack.com. add this page to the navbar"},
     ]}, debug=True)
      
     #  Add a 'programmer' agent who takes the output from the Project Manager and updates the relevant files.
